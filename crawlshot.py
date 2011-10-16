3637b3467c2b594f2ae1d97cfc07c0055d90f165
Erhan Kesken <erhan@kesken.org>
2011-10-16 10:26:50 +0300

    made a simple single thread implementation for hashbang applications with no url path

--- crawlshot.py
+++ crawlshot.py
@@ -3,49 +3,70 @@ import sys
 import os
 import re
 import logging
+from urlparse import urlparse
 
-logging.basicConfig(format='[%(asctime)s][%(levelname)s] %(message)s', filename='crawlshot.log', level=logging.DEBUG)
+# logging.basicConfig(format='[%(asctime)s][%(levelname)s] %(message)s', filename='crawlshot.log', level=logging.DEBUG)
+logging.basicConfig(format='[%(asctime)s][%(levelname)s] %(message)s', level=logging.INFO)
 
 class CrawlShot:
 
-    def __init__(self, webfolder = "www"):
+    def __init__(self, webfolder = "www", testmode = False):
+        self.domain = None
         self.webfolder = webfolder
+        self.testmode = testmode
 
-    def snapshot(self, newURLs, fetchedURLs = []):
+    def snapshot(self, newURLs, fetchedURLFragments = []):
         if newURLs is None or len(newURLs) == 0:
             return 0
-        fetchedURLs = fetchedURLs[:]
-        logging.debug("URLs: %s, fetchedURLs: %s" % (newURLs, fetchedURLs))
+        fetchedURLFragments = fetchedURLFragments[:]
+        # logging.debug("URLs: %s, fetchedURLFragments: %s" % (newURLs, fetchedURLFragments))
         processedURLCount = 0
-        for newURL in newURLs:
-            if newURL in fetchedURLs:
-                logging.debug("URL-'%s' was fetched before", newURL)
+        if len(newURLs) == 0:
+            return 0
+        if self.domain == None:
+            self.domain = urlparse(newURLs[0]).netloc
+            logging.debug("first URL is '%s', setting valid domain as '%s'" % (newURLs[0], self.domain))
+        newURLFragments = [urlparse(newURL).fragment[1:] for newURL in newURLs if newURL.find("#!") != -1 and (urlparse(newURL).netloc == self.domain or urlparse(newURL).netloc == '')]
+        logging.debug("found %d valid URLs to process" % (len(newURLFragments)))
+        # ignored url path, because we don't need it for now
+        # stripped ! character from fragment
+        if len(newURLFragments) == 0:
+            logging.warn("only URLs with #! hashbang are valid!")
+            return 0
+        for newURLFragment in newURLFragments:
+            if newURLFragment in fetchedURLFragments:
+                logging.debug("URL-'%s' was fetched before", newURLFragment)
                 continue
+            newURL = "http://" + self.domain + "#!" + newURLFragment
             logging.info("fetching URL-'%s'" % (newURL))
-            fetchedURLs.append(newURL)
-            # response = os.popen("phantomjs phantomjs/snapshot.js '%s'" % (newURL)).read()
-            response = newURL
-            self.saveResponse(newURL, response)
+            fetchedURLFragments.append(newURLFragment)
+            if self.testmode == True:
+                response = newURLFragment
+            else:
+                response = os.popen("phantomjs phantomjs/snapshot.js '%s'" % (newURL)).read()
+            self.saveResponse(newURLFragment, response)
             foundURLs = self.extractHrefsFromHTML(response)
-            self.snapshot(foundURLs, fetchedURLs)
+            self.snapshot(foundURLs, fetchedURLFragments)
             processedURLCount += 1
         return processedURLCount
 
     def extractHrefsFromHTML(self, response):
-        foundURLs = []
+        foundURLs = re.findall("href=['\"]([^'\"]*)['\"]", response)
         logging.debug("extracted %d URL" % (len(foundURLs)))
+        # return [urlparse(foundURL).fragment[1:] for foundURL in foundURLs if foundURL.find("#!") != -1] # strips ! character from fragment
         return foundURLs
 
-    def saveResponse(self, requestURL, response):
-        (folder, path) = self.pathFromURL(requestURL)
-        not os.path.isdir(folder) and os.makedirs(folder)
-        logging.debug("saved URL-'%s' response at folder-'%s'" % (requestURL, folder))
+    def saveResponse(self, urlFragment, response):
+        (folderpath, filename) = self.pathFromURLFragment(urlFragment)
+        not os.path.isdir(folderpath) and os.makedirs(folderpath)
+        f = open(folderpath + "/" + filename, "w")
+        f.write(response)
+        f.close()
+        logging.debug("saved URL-'%s' response at '%s/%s'" % (urlFragment, folderpath, filename))
         return True
 
-    def pathFromURL(self, requestURL):
-        folderpath = os.path.dirname(re.sub(".*#!", "", requestURL))
-        filename = os.path.basename(requestURL)
-        return (self.webfolder + "/" + folderpath, filename)
+    def pathFromURLFragment(self, urlFragment):
+        return (self.webfolder + "/" + urlFragment, "index.html")
 
 if __name__ == '__main__':
     URLs = sys.argv[1:]
--- testcrawlshot.py
+++ testcrawlshot.py
@@ -1,36 +1,51 @@
+#!/usr/bin/python
+
 import unittest
 import os
+import shutil
 from crawlshot import CrawlShot
 
 
 class CrawlShotTest(unittest.TestCase):
 
     def setUp(self):
+        self.instance = CrawlShot(webfolder="testwww", testmode=True)
+        os.path.isdir("testwww") and shutil.rmtree("testwww")
+
+    def tearDown(self):
         self.instance = CrawlShot("testwww")
+        os.path.isdir("testwww") and shutil.rmtree("testwww")
 
     def testSnapshot(self):
         self.assertEqual(self.instance.snapshot(None), 0)
         self.assertEqual(self.instance.snapshot([]), 0)
-        self.assertEqual(self.instance.snapshot(["http://google.com"]), 1)
-        self.assertEqual(self.instance.snapshot(["http://google.com", "http://hede.com"]), 2)
-        self.assertEqual(self.instance.snapshot(["http://google.com", "http://hede.com", "http://google.com"]), 2)
+        self.assertEqual(self.instance.snapshot(["http://example.com"]), 0)
+        self.assertEqual(self.instance.snapshot(["http://example.com/#!/foo", "http://hede.com/#!/bar"]), 1)
+        self.assertEqual(self.instance.snapshot(["http://example.com/#!/foo", "http://hede.com/#!/bar", "http://example.com/#!/foo"]), 1)
+        self.assertEqual(self.instance.snapshot(["http://example.com/#!/foo", "http://hede.com/#!/bar", "http://example.com/#!/foo", "http://example.com/#!/bar"]), 2)
 
     def testPathFromURL(self):
-        self.assertEqual(self.instance.pathFromURL("foo/--/bar/hede"), ("testwww/foo/--/bar", "hede"))
-        self.assertEqual(self.instance.pathFromURL("!foo/--/bar/hede"), ("testwww/!foo/--/bar", "hede"))
-        self.assertEqual(self.instance.pathFromURL("#foo/--/bar/hede"), ("testwww/#foo/--/bar", "hede"))
-        self.assertEqual(self.instance.pathFromURL("#!foo/--/bar/hede"), ("testwww/foo/--/bar", "hede"))
-        self.assertEqual(self.instance.pathFromURL("#!/foo/--/bar/hede"), ("testwww//foo/--/bar", "hede"))
-        self.assertEqual(self.instance.pathFromURL("/#!/foo/--/bar/hede"), ("testwww//foo/--/bar", "hede"))
-        self.assertEqual(self.instance.pathFromURL("http://example.com#!foo/--/bar/hede"), ("testwww/foo/--/bar", "hede"))
-        self.assertEqual(self.instance.pathFromURL("http://example.com#!/foo/--/bar/hede"), ("testwww//foo/--/bar", "hede"))
-        self.assertEqual(self.instance.pathFromURL("http://example.com/#!/foo/--/bar/hede"), ("testwww//foo/--/bar", "hede"))
+        self.assertEqual(self.instance.pathFromURLFragment("foo/--/bar/hede"), ("testwww/foo/--/bar/hede", "index.html"))
+        self.assertEqual(self.instance.pathFromURLFragment("/foo/--/bar/hede"), ("testwww//foo/--/bar/hede", "index.html"))
+
+    def testExtractHrefsFromHTML(self):
+        extractedURLs = self.instance.extractHrefsFromHTML("<a href='#!/foo/--/bar/hede'>hede</a>")
+        self.assertEqual(len(extractedURLs), 1)
+        self.assertEqual(extractedURLs[0], "#!/foo/--/bar/hede")
+        extractedURLs = self.instance.extractHrefsFromHTML("<a href='#!/foo/--/bar/hede'>hede</a><a href='#!/bar/--/bar/hede'>hede</a><a href=\"#!/aaa/--/bar/hede\">hede</a>")
+        self.assertEqual(len(extractedURLs), 3)
+        self.assertEqual(extractedURLs[0], "#!/foo/--/bar/hede")
+        self.assertEqual(extractedURLs[1], "#!/bar/--/bar/hede")
+        self.assertEqual(extractedURLs[2], "#!/aaa/--/bar/hede")
+        extractedURLs = self.instance.extractHrefsFromHTML("<a href='#!/bar/--/bar/hede'>hede</a><br />\r\n<a href=\"#!/aaa/--/bar/hede\">hede</a>")
+        self.assertEqual(len(extractedURLs), 2)
+        self.assertEqual(extractedURLs[0], "#!/bar/--/bar/hede")
+        self.assertEqual(extractedURLs[1], "#!/aaa/--/bar/hede")
 
     def testSaveResponse(self):
         testfolderpath = "testwww/foo/--/bar"
-        os.path.isdir(testfolderpath) and os.removedirs(testfolderpath)
         self.assertFalse(os.path.isdir(testfolderpath))
-        self.assertTrue(self.instance.saveResponse("#!/foo/--/bar/hede", ""))
+        self.assertTrue(self.instance.saveResponse("/foo/--/bar/hede", "hede"))
         self.assertTrue(os.path.isdir(testfolderpath))
 
 if __name__ == '__main__':
